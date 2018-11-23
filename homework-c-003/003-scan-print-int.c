6f70c03bedb802e4d33ce3ddc59799ad2f2a521b
Hudhfchdjh <40856140+Hudhfchdjh@users.noreply.github.com>
2018-11-23 19:18:32 +0200

    Add files via upload

---
+++ homework-c-003/003-scan-print-int.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int number;/*ініціалізація змінної*/
+    printf("Enter an integer: ");  /*вивод рядка*/
+    
+    scanf("%d", &number);  /*введення в рядок введення*/
+    
+    printf("You entered: %d", number);/*вивод рядка*/
+    return 0;
+}
